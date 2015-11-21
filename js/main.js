1fc03a9d6fdaabee0716083441516847f865e4e3
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-11-21 10:08:10 -0800

    feat: Add google map

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
+var locations = [];
+
+var googleMap;
+function initMap() {
+	googleMap = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 44.635371, lng: -124.053291},
+	zoom: 14
+	});
+}
 
 
-var fourSquareUrl = 'https://api.foursquare.com/v2/venues/search?client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20130815&ll=44.634589, -124.053299&query=coffee'
-$.ajax({
-	url: fourSquareUrl,
-	async: true,
-	dataType: "json",
-	success: function (data) {
-		console.log(data);
-	}
-});
+var ViewModel = function(){
+	var self = this;
+
+	var fourSquareUrl = 'https://api.foursquare.com/v2/venues/search?client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20130815&near=Newport, OR&radius=7500&query=coffee';
+	$.ajax({
+		url: fourSquareUrl,
+		async: true,
+		dataType: "json",
+		success: function (data) {
+			locations.push(data.response.venues);
+			console.log(locations);
+		}
+	});
+
+};
+
+//var ViewModel = new ViewModel();
+ko.applyBindings(new ViewModel());
 
 
 
-// $.getJSON(fourSquareUrl, function (data) {
-// 	console.log(data);
-// });
+
+
