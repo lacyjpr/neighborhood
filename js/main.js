d2c37fb55df589e2304c747c4df131df4d4d65d1
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-11-22 09:26:02 -0800

    fix: Attempt to access AJAX data outside requestß

--- js/main.js
+++ js/main.js
@@ -26,23 +26,40 @@ var ViewModel = function(){
 	"use strict";
 	var self = this;
 
+
+
 	var fourSquareUrl = 'https://api.foursquare.com/v2/venues/search?client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20130815&near=Newport, OR&radius=7500&query=coffee';
 	$.ajax({
 		url: fourSquareUrl,
 		async: true,
 		dataType: "json",
 		success: function (data) {
+			var data = data;
 			console.log(data);
 			locations.push(data.response.venues);
+
+			// Save data outside the AJAX request
+			saveFourSquare(data);
+
+			// All work here
 			console.log(locations);
 			console.log(locations[0].length)
 			console.log(locations[0][0].location);
 		}
 	});
 
+	function saveFourSquare(data) {
+		locations.push(data.response.venues);
+
+		// Works here
+		console.log(locations);
+	}
+
+// Doesn't work here
 console.log(locations);
 
-console.log(this.locations[0].length);
+// Doesn't work here
+console.log(locations[0].length);
 for (var i = 0; i < locations[0].length; i++) {
 		console.log("lat" + locations[0][i].location.lat)
 		console.log("lng" + locations[0][i].location.lng)
