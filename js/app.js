9a0143f4cdef5e6ed8b09debf92165b3950d837d
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-11-24 21:33:23 -0800

    New app.js

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var locations = [
+{
+	name: "Fast Lane Coffee",
+	lat: 44.64070053,
+	lng: -124.05267986
+},
+{
+	name: "Dutch Bros. Coffee",
+	lat: 44.62933649650506,
+	lng: -124.06172633171082
+},
+{
+	name: "The Coffee House",
+	lat: 44.631362557411194,
+	lng: -124.050916
+},
+{
+	name: "Solid Grounds Coffee House",
+	lat: 44.633673,
+	lng: -124.057304
+},
+{
+	name: "Bayscapes Coffee House",
+	lat: 44.63016470161697,
+	lng: -124.05276088349126
+},
+{
+	name: "Carls Coffee",
+	lat: 44.638854379944675,
+	lng: -124.0611189933332
+},
+
+
+
+
+
+]
