85b41d5e971d10112d270c004fdba8de130e474c
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-11-27 18:11:25 -0800

    fix: Fix filtering

--- js/app.js
+++ js/app.js
@@ -61,12 +61,12 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 44.635371, lng: -124.053291},
-    //center: {lat: 44.64070053, lng: -124.05267986},
     zoom: 14
   });
 ko.applyBindings(new ViewModel()); 
 }
 
+// Alert the user if google maps isn't working
 function googleError() {
 	"use strict";
 	document.getElementById('map').innerHTML = "<h1>Google Maps is not loading</h1>";
@@ -84,15 +84,18 @@ var Place = function(data) {
 var ViewModel = function(){
 	var self = this;
 
+	// Create an array of all places
 	// Credit https://www.udacity.com/course/viewer#!/c-ud989-nd/l-3406489055/e-3464818693/m-3464818694
 	this.placeList = ko.observableArray([]);
 
 	// Create Place objects for each item in locations
+	// Credit https://www.udacity.com/course/viewer#!/c-ud989-nd/l-3406489055/e-3464818693/m-3464818694
 	locations.forEach(function(placeItem) {
 		self.placeList.push( new Place(placeItem));
 	}); 
 	console.dir(self.placeList);
 	
+	// Place markers
 	// Credit https://github.com/kacymckibben/project-5-app.git
 	var infowindow = new google.maps.InfoWindow();
 	var marker;
@@ -105,47 +108,53 @@ var ViewModel = function(){
 			animation: google.maps.Animation.DROP,
 			title: placeItem.name()
 		});
-
-		//Add infowindows credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
-			google.maps.event.addListener(placeItem.marker, 'click', function () {
-			infowindow.open(map, this);
-			marker.setMap(map);
-			});
 		placeItem.marker = marker;
 	});
 
+	function toggleBounce() {
+		if(placeItem.marker.getAnimation() !== null) {
+			placeItem.marker.setAnimation(null);
+		} else {
+			placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
+		}
+	}
 
-	// Array with only the markers that should be visible based on search
-	// Credit http://codepen.io/prather-mcs/pen/KpjbNN?editors=001
+		//Add infowindows credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+		google.maps.event.addListener(marker, 'click', function () {
+		infowindow.open(map, this);
+		});
+
+	// Array containing only the markers based on search
 	self.visible = ko.observableArray();
 
-	// All places are visible by default before user input
+	// All markers are visible by default before any user search
 	self.placeList().forEach(function(place) {
 		self.visible.push(place);
 	});
-	console.dir(self.visible);
 
-	// Track user input
+	// Keep track of user input
 	self.userInput = ko.observable('');
 
-	// Filter markers: Remove all visible places, 
-	// if user input matches a place, make it and its marker visible
+	// Filter markers: Set all markers to not visible. 
+	// Only display them if they match user search input
+	// Credit http://codepen.io/prather-mcs/pen/KpjbNN?editors=001
 	self.filterMarkers = function() {
 		var searchInput = self.userInput().toLowerCase();
-
 		self.visible.removeAll();
-		// Check places for match with user input
+		// Compare the name of each place to the user input
+		// If it matches, display the place and marker
 		self.placeList().forEach(function(place) {
 			place.marker.setVisible(false);
-
-		if (place().name.toLowerCase().indexOf(searchInput) !== -1) {
+		if (place.name().toLowerCase().indexOf(searchInput) !== -1) {
 			self.visible.push(place);
 			}
 		});
 		self.visible().forEach(function(place){
 			place.marker.setVisible(true);
-		});
-	}; 
+		})
+	}
+
+
 
 
 
