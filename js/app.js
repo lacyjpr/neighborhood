4e59428d142ec236be06f5f8df8fe73b22da1fda
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-11-28 16:14:12 -0800

    fix: Attempt fix of hasOwnProperty

--- js/app.js
+++ js/app.js
@@ -132,7 +132,7 @@ var ViewModel = function(){
 					placeItem.name(result.name);
 					//placeItem.phone(result.contact.formattedPhone);
 					// Credit https://discussions.udacity.com/t/foursquare-results-undefined-until-the-second-click-on-infowindow/39673/2
-					var contact = venue.hasOwnProperty('contact') ? venue.contact : '';
+					var contact = result.hasOwnProperty('contact') ? venue.contact : '';
 					if (contact.hasOwnProperty('formattedPhone')) {
     					placeItem.phone(contact.formattedPhone || '');
 					}
