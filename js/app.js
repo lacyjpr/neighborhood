49e1854bcd4e7a3d8a5519007f0816d0d87461f1
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-12-01 21:36:56 -0800

    fix: Fix broken Foursquare url

--- js/app.js
+++ js/app.js
@@ -132,7 +132,7 @@ var ViewModel = function(){
 		placeItem.marker = marker;
 
 			$.ajax({
-				url: 'https://api.oursquare.com/v2/venues/' + placeItem.id() + '?client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20130815',
+				url: 'https://api.foursquare.com/v2/venues/' + placeItem.id() + '?client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20130815',
 				dataType: "json",
 				success: function(data) {
 					var result = data.response.venue;
