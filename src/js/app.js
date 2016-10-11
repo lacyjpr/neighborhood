a742a6f593d283d7f254c315785ed131a7ec222e
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-10-11 00:49:09 -0700

    change file structure

---
+++ src/js/app.js
@@ -0,0 +1,134 @@
+var locations = [
+	{
+		name: "Fast Lane Coffee",
+		lat: 44.64070053,
+		lng: -124.05267986
+	},
+	{
+		name: "Dutch Bros. Coffee",
+		lat: 44.62933649650506,
+		lng: -124.06172633171082
+	},
+	{
+		name: "The Coffee House",
+		lat: 44.631362557411194,
+		lng: -124.050916
+	},
+	{
+		name: "Solid Grounds Coffee House",
+		lat: 44.633673,
+		lng: -124.057304
+	},
+	{
+		name: "Bayscapes Coffee House",
+		lat: 44.63016470161697,
+		lng: -124.05276088349126
+	},
+	{
+		name: "Carls Coffee",
+		lat: 44.638854379944675,
+		lng: -124.0611189933332
+	},
+	{
+		name: "Dockside Coffee House Gallery",
+		lat: 44.630096,
+		lng: -124.05267119407654
+	},
+	{
+		name: "Surf Town Coffee Company",
+		lat: 44.62982165943548,
+		lng: -124.05335751403604
+	},
+	{
+		name: "Starbucks",
+		lat: 44.63738090498442,
+		lng: -124.05263566533459
+	},
+	{
+		name: "Fins Coffee at the Oregon Coastal Aquarium",
+		lat: 44.617662114530646,
+		lng: -124.04709191325138
+	},
+	{
+		name: "Starbucks",
+		lat: 44.651158892548324,
+		lng: -124.0517664026574
+	}
+];
+
+// Initialize the map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 44.635371, lng: -124.053291},
+    //center: {lat: 44.64070053, lng: -124.05267986},
+    zoom: 14
+  });
+ko.applyBindings(new ViewModel()); 
+}
+
+function googleError() {
+	"use strict";
+	document.getElementById('map').innerHTML = "<h1>Google Maps is not loading</h1>";
+};
+
+//Place constructor
+// Credit https://discussions.udacity.com/t/having-trouble-accessing-data-outside-an-ajax-request/39072/10
+var Place = function(data) {
+	this.name = ko.observable(data.name);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
+	this.marker = ko.observable();
+}
+
+var ViewModel = function(){
+	var self = this;
+
+	// Credit https://www.udacity.com/course/viewer#!/c-ud989-nd/l-3406489055/e-3464818693/m-3464818694
+	this.placeList = ko.observableArray([]);
+
+	// Create Place objects for each item in locations
+	locations.forEach(function(placeItem) {
+		self.placeList.push( new Place(placeItem));
+	}); 
+	console.dir(self.placeList);
+	
+	// Credit https://github.com/kacymckibben/project-5-app.git
+	var infowindow = new google.maps.InfoWindow();
+	var marker;
+
+	self.placeList().forEach(function(placeItem){
+
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(placeItem.lat(),placeItem.lng()),
+			map: map,
+			animation: google.maps.Animation.DROP,
+			title: placeItem.name()
+		});
+		//Add infowindows credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+			google.maps.event.addListener(marker, 'click', function () {
+			infowindow.open(map, this);
+			marker.setMap(map);
+			});
+		placeItem.marker = marker;
+	});
+
+
+// Array with only the markers that should be visible based on search
+// Credit http://codepen.io/prather-mcs/pen/KpjbNN?editors=001
+	self.visible = ko.observableArray();
+
+	self.placeList().forEach(function(place) {
+		self.visible.push(place);
+	});
+	console.dir(self.visible);
+
+
+
+} // ViewModel
+
+
+
+
+
+
---
+++ src/js/oldmain.js
@@ -0,0 +1,83 @@
+var locations = [];
+
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 44.635371, lng: -124.053291},
+    zoom: 14
+  });
+  infowindow = new google.maps.InfoWindow({
+	content: name
+	});
+}
+
+
+
+function googleError() {
+	"use strict";
+	document.getElementById('map').innerHTML = "<h1>Google Maps is not loading</h1>";
+};
+
+
+var ViewModel = function(){
+	"use strict";
+	var self = this;
+
+	self.venues = ko.observableArray();
+	console.dir(self.venues);
+
+	function getNames(data) {
+		locations.push(data.response.venues);
+		for (var i = 0; i < locations[0].length; i++) {
+			self.venues.push(locations[0][i]);
+			}
+		};
+
+	var fourSquareUrl = 'https://api.foursquare.com/v2/venues/search?client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20130815&near=Newport, OR&radius=7500&query=coffee';
+	$.ajax({
+		url: fourSquareUrl,
+		async: true,
+		dataType: "json",
+		success: function (data) {
+			locations.push(data.response.venues);
+			console.log(locations);
+
+
+			// Set Markers with data
+			setMarkers(data);
+			getNames(data);
+		}
+	});
+
+	function setMarkers(data) {
+		"use strict";
+		locations.push(data.response.venues);
+		for (var i = 0; i < locations[0].length; i++) {
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[0][i].location.lat, locations[0][i].location.lng),
+			map: map,
+			title: locations[0][i].name,
+			});
+		//Add infowindows credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+		google.maps.event.addListener(marker, 'click', function () {
+		infowindow.open(map, this);
+		marker.setMap(map);
+		});
+
+
+
+
+	};
+
+
+	}
+	}
+
+ko.applyBindings(new ViewModel());
+
+
+
+
+
